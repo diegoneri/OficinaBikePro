e12a8565c38961c7f61e069e2cb90c8553033028
Diego Neri Felix <Diego Neri>
2025-04-28 08:33:31 -0300

    Inicio do catálogo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+string nomePrimeiroItem = "Câmara de ar";
+string segundoPrimeiroItem = "Capacete";
+string terceiroPrimeiroItem = "Kit de Ferramentas";
+
+decimal precoPrimeiroItem = 29.90m;
+decimal precoSegundoItem = 134.90m;
+decimal precoTerceiroItem = 189.00m;
+
+double percentualImpostoVenda = 0.088; //8,8%
+
+const int TAMANHO_LINHA = 30;
+const char SEPARADOR = '=';
+
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+Console.WriteLine($"{"|".PadRight(7)}Oficina Bike Pro{"|".PadLeft(8)}");
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+Console.WriteLine($"{"|".PadRight(11)}CATÁLOGO{"|".PadLeft(12)}");
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+
