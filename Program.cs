3d73c7f308ad75429864beba49a7ed0b11129525
Diego Neri Felix <Diego Neri>
2025-04-28 08:59:46 -0300

    Realiza os cálculos do programa

--- Program.cs
+++ Program.cs
@@ -1,21 +1,54 @@
 ﻿Console.Clear();
 
 string nomePrimeiroItem = "Câmara de ar";
-string segundoPrimeiroItem = "Capacete";
-string terceiroPrimeiroItem = "Kit de Ferramentas";
+string nomeSegundoItem = "Capacete";
+string nomeTerceiroItem = "Kit de Ferramentas";
 
 decimal precoPrimeiroItem = 29.90m;
 decimal precoSegundoItem = 134.90m;
 decimal precoTerceiroItem = 189.00m;
 
-double percentualImpostoVenda = 0.088; //8,8%
+decimal percentualImpostoVenda = 0.088m; //8,8%
 
-const int TAMANHO_LINHA = 30;
+int qtdItem1, qtdItem2, qtdItem3;
+
+decimal subtotalItemUm, subTotalItemDois, 
+        subTotalItemTres, totalClienteUm,
+        valorDoImposto, totalComImposto;
+
+
+const int TAMANHO_LINHA = 36;
 const char SEPARADOR = '=';
 
 Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
-Console.WriteLine($"{"|".PadRight(7)}Oficina Bike Pro{"|".PadLeft(8)}");
+Console.WriteLine($"{"|".PadRight(10)}Oficina Bike Pro{"|".PadLeft(11)}");
 Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
-Console.WriteLine($"{"|".PadRight(11)}CATÁLOGO{"|".PadLeft(12)}");
+Console.WriteLine($"{"|".PadRight(14)}CATÁLOGO{"|".PadLeft(15)}");
 Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
 
+Console.WriteLine($"{"| ITEM",-24}{"VALOR",-12}|");
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+
+Console.WriteLine($"| 1. { nomePrimeiroItem,-18} {precoPrimeiroItem,-12:C2}|");
+Console.WriteLine($"| 2. { nomeSegundoItem,-18} {precoSegundoItem,-12:C2}|");
+Console.WriteLine($"| 3. { nomeTerceiroItem,-18} {precoTerceiroItem,-12:C2}|");
+
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+Console.WriteLine($"{"| Imposto sobre a venda: 8,8%",-TAMANHO_LINHA}|");
+Console.WriteLine($"{"+".PadRight(TAMANHO_LINHA, SEPARADOR)}+");
+
+Console.Write($"\n\nInforme a quantidade de {nomePrimeiroItem}: ");
+qtdItem1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Informe a quantidade de {nomeSegundoItem}: ");
+qtdItem2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Informe a quantidade de {nomeTerceiroItem}: ");
+qtdItem3 = Convert.ToInt32(Console.ReadLine());
+
+subtotalItemUm = qtdItem1 * precoPrimeiroItem;
+subTotalItemDois = qtdItem2 * precoSegundoItem;
+subTotalItemTres = qtdItem3 * precoTerceiroItem;
+
+totalClienteUm = subtotalItemUm + subTotalItemDois + subTotalItemTres;
+valorDoImposto = totalClienteUm * percentualImpostoVenda;
+
+totalComImposto = totalClienteUm + valorDoImposto;
